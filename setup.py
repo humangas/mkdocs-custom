a84905209b5cdded54e8ac9f34bb1c2c400bdb4b
humangas <onigiri@humangas.net>
2016-07-24 13:26:33 +0900

    add copy assts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='mkdocs-custom',
     version='0.1.0',
-    description='mkdocs-custom',
+    description='Copy mkdocs assts from mkdocs-custom.',
     author='humangas',
     url='https://github.com/humangas/mkdocs-custom',
     packages=['mkdocs-custom'],
@@ -15,4 +15,24 @@ setup(
     ],
 )
 
-# mkdocs-custom, mkdocs 配下パス同じで置き換える。同じファイルが先にあれば削除する 
+
+from distutils.sysconfig import get_python_lib
+import shutil, os
+
+def get_contains_file_dirpaths(root_dir_path):
+    """Return contains file dirpaths."""
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(root_dir_path)
+            if len(filenames) > 0]
+
+def copy_mkdocs_assets():
+    """Copy mkdocs assts from mkdocs-custom."""
+    root_dir_path = os.path.join(get_python_lib(), 'mkdocs-custom')
+    copy_files_dirs = get_contains_file_dirpaths(root_dir_path)
+
+    for dir in copy_files_dirs:
+        dst = dir.replace('mkdocs-custom', 'mkdocs')
+        for file in os.listdir(dir):
+            shutil.copy(os.path.join(dir, file), dst)
+
+copy_mkdocs_assets()
