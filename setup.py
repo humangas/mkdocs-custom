9897f21290461b12d8ebfc65ee7749105205aead
humangas <onigiri@humangas.net>
2016-07-24 12:29:08 +0900

    パス被ってたら上書きされないので、ルートパス変更

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     description='mkdocs-custom',
     author='humangas',
     url='https://github.com/humangas/mkdocs-custom',
-    packages=['mkdocs'],
+    packages=['mkdocs-custom'],
     include_package_data=True,
     install_requires=[
         'mkdocs>=0.15.3',
     ],
 )
+
+# mkdocs-custom, mkdocs 配下パス同じで置き換える。同じファイルが先にあれば削除する 
